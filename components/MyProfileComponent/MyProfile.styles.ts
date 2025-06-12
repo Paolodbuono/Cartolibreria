cae9327bc5841b2b45630dc667ccf5405bc443cc
Paolo Dello Buono <paolodellobuono@gmail.com>
2025-06-12 20:18:05 +0200

    Fix width is not defined

--- components/MyProfileComponent/MyProfile.styles.ts
+++ components/MyProfileComponent/MyProfile.styles.ts
@@ -1,6 +1,8 @@
 import { bg, md } from '@/constants/FontSize';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+const { width } = Dimensions.get('window');
 
 const isTablet = width > 768;
 
