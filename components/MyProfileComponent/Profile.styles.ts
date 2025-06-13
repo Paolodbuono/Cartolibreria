d2b19c071979b008120e74f52e2a136dd77cd67b
Paolo Dello Buono <paolodellobuono@gmail.com>
2025-06-13 15:12:54 +0200

    Fix Riepilogo die tuoi dati style

--- components/MyProfileComponent/Profile.styles.ts
+++ components/MyProfileComponent/Profile.styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export const styles = StyleSheet.create({
-    wrapper: { width: wp("100%"), padding: wp("5%"), height: "100%" },
+    wrapper: { width: wp("90%"), padding: wp("5%"), height: "100%" },
     title: { fontSize: bg, padding: 10, fontWeight: '400', color: '#EB5F19' },
     titleBOLD: { fontSize: bg, padding: 10, fontWeight: '800', color: '#EB5F19' },
     subTitle: { fontSize: md, fontWeight: '400', color: '#4975be', top: -5 },
