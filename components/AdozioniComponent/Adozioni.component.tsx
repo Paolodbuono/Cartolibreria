bdd09ae388bd141e632e56fc3e34007138333c89
Paolo Dello Buono <paolodellobuono@gmail.com>
2025-07-11 17:43:06 +0200

    Update Perche Sceglierci

--- components/AdozioniComponent/Adozioni.component.tsx
+++ components/AdozioniComponent/Adozioni.component.tsx
@@ -261,7 +261,7 @@ export const AdozioniComponent = ({ }) => {
                                                 styles.sedeImage,
                                                 state.lockSede && state.sedeSelezionata === SEDI[0] && styles.disabledImage,
                                             ]}
-                                            source={require('@/assets/images/sedePompei.jpg')}
+                                            source={require('@/assets/images/sedePompei.png')}
                                             resizeMode="cover"
                                         />
                                     </View>
