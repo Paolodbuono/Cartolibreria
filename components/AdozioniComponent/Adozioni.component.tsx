43bcb5e870c7f0823cdfb03176f821c7e774fbc6
Paolo Dello Buono <paolodellobuono@gmail.com>
2025-07-18 10:05:05 +0200

    Update App Version for Libri Già Posseduti

--- components/AdozioniComponent/Adozioni.component.tsx
+++ components/AdozioniComponent/Adozioni.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Image, Text, ActivityIndicator as Spinner, TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaView, View, Image, Text, ActivityIndicator as Spinner, TouchableOpacity, ScrollView, Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Picker } from '@react-native-picker/picker';
 
@@ -10,6 +10,10 @@ import TextComponent from '../Commons/Text.component';
 import { CustomProgressStep } from './CustomProgressStep';
 import { CustomProgressSteps } from './CustomProgressSteps';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+const isWeb = Platform.OS === "web";
+
+const isTablet = wp('100%') > 600; // Condizione per determinare se è tablet
 
 interface State {
     nextButtonFirstStepEnabled: boolean;
@@ -28,14 +32,14 @@ interface State {
     corsiPickerItem: string[];
     sezioniPickerItem: string[];
     classiPickerItem: string[];
-    availableSchoolsIds: number[];
     coursesName: string[];
     sectionsName: string[];
     classesName: string[];
-    books: any[];
     lockSede: boolean;
     selectedCitta: string;
     selectedScuola: string
+    LibriDaAcquistare: any[];
+    LibriPosseduti: any[];
 }
 
 const initialState: State = {
@@ -55,11 +59,11 @@ const initialState: State = {
     corsiPickerItem: [],
     sezioniPickerItem: [],
     classiPickerItem: [],
-    availableSchoolsIds: [],
     coursesName: [],
     sectionsName: [],
     classesName: [],
-    books: [],
+    LibriDaAcquistare: [],
+    LibriPosseduti: [],
     lockSede: false,
     selectedCitta: '',
     selectedScuola: ''
@@ -74,7 +78,7 @@ export const AdozioniComponent = ({ }) => {
         updateState({ isLoading: true })
         try {
 
-            const sedeSelezionata = await AsyncStorage.getItem('sedeSelezionata')
+            const sedeSelezionata = isWeb ? localStorage.getItem('sedeSelezionata') : await AsyncStorage.getItem('sedeSelezionata')
 
             if (SEDI[0] === sedeSelezionata || SEDI[1] === sedeSelezionata) {
                 updateState({ sedeSelezionata: sedeSelezionata, nextButtonFirstStepEnabled: false, lockSede: true });
@@ -133,11 +137,11 @@ export const AdozioniComponent = ({ }) => {
             updateState({ showLoadingBooks: true });
             console.log("state", state)
 
-            const url = `https://www.libreriabonagura.it/micro/getBooks.asp?libreria=${state.sedeSelezionata}&school=${state.availableSchoolsIds[state.selectedIdxScuola ?? 0]}&type=${encodeURIComponent(state.coursesName[state.selectedIdxCorso])}&class=${state.classesName[state.selectedIdxClasse]}&section=${state.sectionsName[state.selectedIdxSezione]}`;
+            const url = `https://www.libreriabonagura.it/micro/getBooks.asp?libreria=${state.sedeSelezionata}&school=${state.selectedIdxScuola}&type=${encodeURIComponent(state.coursesName[state.selectedIdxCorso])}&class=${state.classesName[state.selectedIdxClasse]}&section=${state.sectionsName[state.selectedIdxSezione]}`;
             const response = await fetch(url);
             const booksData = await response.json();
 
-            updateState({ books: booksData.data, showLoadingBooks: false });
+            updateState({ LibriDaAcquistare: booksData.LibriDaAcquistare, showLoadingBooks: false, LibriPosseduti: booksData.LibriPosseduti });
         } catch (error) {
             console.error('Errore durante la richiesta dei libri:', error);
         }
@@ -166,10 +170,9 @@ export const AdozioniComponent = ({ }) => {
 
                 updateState({
                     nomiScuolePickerItem: tempSchoolsName.map(el => <Picker.Item key={el.id} label={el.nome} value={JSON.stringify(el)} />),
-                    availableSchoolsIds: tempSchoolsIds
                 });
 
-                await fetchOtherInfo(tempSchoolsIds[0]);
+                await fetchOtherInfo(tempSchoolsIds[0], tempSchoolsName[0].nome);
             }
         } catch (error) {
             console.error('Errore durante la richiesta delle scuole:', error);
@@ -178,7 +181,7 @@ export const AdozioniComponent = ({ }) => {
         }
     };
 
-    const fetchOtherInfo = async (id: number) => {
+    const fetchOtherInfo = async (id: number, itemValue: string) => {
         try {
             updateState({ isLoadingOtherInfo: true });
 
@@ -198,7 +201,12 @@ export const AdozioniComponent = ({ }) => {
                 corsiPickerItem: coursesName.data.map((el: { tipo: string | undefined; }) => <Picker.Item key={el.tipo} label={el.tipo} value={el.tipo} />),
                 sezioniPickerItem: sectoinsName.data.map((el: string | undefined) => <Picker.Item key={el} label={el} value={el} />),
                 classiPickerItem: classesName.data.map((el: { classe: string | undefined; }) => <Picker.Item key={el.classe} label={el.classe} value={el.classe} />),
-                isLoadingOtherInfo: false
+                isLoadingOtherInfo: false,
+                selectedIdxScuola: id,
+                selectedScuola: itemValue,
+                selectedIdxCorso: 0,
+                selectedIdxClasse: 0,
+                selectedIdxSezione: 0
             });
 
         } catch (error) {
@@ -231,24 +239,6 @@ export const AdozioniComponent = ({ }) => {
                     >
                         <ScrollView contentContainerStyle={styles.scrollViewContent}>
                             <View style={styles.imagesContainer}>
-                                {/* Sede Poggiomarino */}
-                                <TouchableOpacity
-                                    onPress={() => selectSede(0)}
-                                    disabled={state.lockSede && state.sedeSelezionata === SEDI[1]}
-                                    style={styles.imageWrapper}
-                                >
-                                    <View style={[styles.sedeContainer, state.sedeSelezionata === SEDI[0] && styles.selectedSede]}>
-                                        <Image
-                                            style={[
-                                                styles.sedeImage,
-                                                state.lockSede && state.sedeSelezionata === SEDI[1] && styles.disabledImage,
-                                            ]}
-                                            source={require('@/assets/images/sedePoggiomarino.jpg')}
-                                            resizeMode="cover"
-                                        />
-                                    </View>
-                                </TouchableOpacity>
-
                                 {/* Sede Pompei */}
                                 <TouchableOpacity
                                     onPress={() => selectSede(1)}
@@ -262,6 +252,23 @@ export const AdozioniComponent = ({ }) => {
                                                 state.lockSede && state.sedeSelezionata === SEDI[0] && styles.disabledImage,
                                             ]}
                                             source={require('@/assets/images/sedePompei.png')}
+                                            resizeMode="cover"
+                                        />
+                                    </View>
+                                </TouchableOpacity>
+                                {/* Sede Poggiomarino */}
+                                <TouchableOpacity
+                                    onPress={() => selectSede(0)}
+                                    disabled={state.lockSede && state.sedeSelezionata === SEDI[1]}
+                                    style={styles.imageWrapper}
+                                >
+                                    <View style={[styles.sedeContainer, state.sedeSelezionata === SEDI[0] && styles.selectedSede]}>
+                                        <Image
+                                            style={[
+                                                styles.sedeImage,
+                                                state.lockSede && state.sedeSelezionata === SEDI[1] && styles.disabledImage,
+                                            ]}
+                                            source={require('@/assets/images/sedePoggiomarino.jpg')}
                                             resizeMode="cover"
                                         />
                                     </View>
@@ -306,8 +313,7 @@ export const AdozioniComponent = ({ }) => {
                                         selectedValue={state.selectedScuola}
                                         onValueChange={(itemValue) => {
                                             const item = JSON.parse(itemValue);
-                                            updateState({ selectedIdxScuola: item.id, selectedScuola: itemValue });
-                                            fetchOtherInfo(item.id);
+                                            fetchOtherInfo(item.id, itemValue);
                                         }}>
                                         {state.nomiScuolePickerItem}
                                     </Picker>
@@ -347,20 +353,59 @@ export const AdozioniComponent = ({ }) => {
                     </CustomProgressStep>
 
                     <CustomProgressStep label="Lista Libri" previousBtnText="Indietro" onPrevious={() => null}>
-                        <ScrollView>
-                            {state.showLoadingBooks && <Spinner size="large" style={gs.spinner} />}
-                            {!state.showLoadingBooks && state.books.map((book, idx) => (
-                                <View key={idx}>
-                                    <Image style={styles.bookImage} source={{ uri: `https://www.libreriabonagura.it/wbresize.aspx?f=${book.isbn}.jpg&c=100&w=150` }} />
-                                    <View>
-                                        <Text>{book.titolo}</Text>
-                                        <Text>{book.autore}</Text>
-                                        <Text>Nuovo: {book.prezzo.toFixed(2)}€</Text>
-                                        <Text>Usato: {getPrezzoUsato(book.prezzo)}</Text>
-                                    </View>
+                        {state.showLoadingBooks && <Spinner size="large" style={gs.spinner} />}
+                        {!state.showLoadingBooks && <>
+                            {Platform.OS === 'web' && isTablet && <>
+                                <View style={styles.listaLibri}>
+                                    {state.LibriDaAcquistare.map((book, idx) => (
+                                        <View key={idx} style={styles.bookItem}>
+                                            <Image style={styles.bookImage} source={{ uri: `https://www.libreriabonagura.it/wbresize.aspx?f=${book.isbn}.jpg&c=100&w=150` }} />
+                                            <Text style={styles.title}>{book.titolo}</Text>
+                                            <Text style={styles.author}>{book.autore}</Text>
+                                            <Text style={styles.price}>Nuovo: {book.prezzo.toFixed(2)}€</Text>
+                                            <Text style={styles.price}>Usato: {getPrezzoUsato(book.prezzo)}</Text>
+                                        </View>
+                                    ))}
+                                    {state.LibriPosseduti.length > 0 && <Text style={{...styles.sezioneTitolo, width: "100%", alignSelf: "stretch"}}>Libri già Posseduti</Text>}
+                                    {state.LibriPosseduti.map((book, idx) => (
+                                        <View key={idx} style={styles.bookItem}>
+                                            <Image style={styles.bookImage} source={{ uri: `https://www.libreriabonagura.it/wbresize.aspx?f=${book.isbn}.jpg&c=100&w=150` }} />
+                                            <Text style={styles.title}>{book.titolo}</Text>
+                                            <Text style={styles.author}>{book.autore}</Text>
+                                            <Text style={styles.price}>Nuovo: {book.prezzo.toFixed(2)}€</Text>
+                                            <Text style={styles.price}>Usato: {getPrezzoUsato(book.prezzo)}</Text>
+                                        </View>
+                                    ))}
                                 </View>
-                            ))}
-                        </ScrollView>
+                            </>}
+                            {(Platform.OS !== 'web' || (Platform.OS === 'web' && !isTablet)) &&
+                                <ScrollView>
+                                    {state.LibriDaAcquistare.map((book, idx) => (
+                                        <View key={idx}>
+                                            <Image style={styles.bookImage} source={{ uri: `https://www.libreriabonagura.it/wbresize.aspx?f=${book.isbn}.jpg&c=100&w=150` }} />
+                                            <View>
+                                                <Text>{book.titolo}</Text>
+                                                <Text>{book.autore}</Text>
+                                                <Text>Nuovo: {book.prezzo.toFixed(2)}€</Text>
+                                                <Text>Usato: {getPrezzoUsato(book.prezzo)}</Text>
+                                            </View>
+                                        </View>
+                                    ))}
+                                    {state.LibriPosseduti.length > 0 && <Text style={styles.sezioneTitolo}>Libri già Posseduti</Text>}
+                                    {state.LibriPosseduti.map((book, idx) => (
+                                        <View key={idx}>
+                                            <Image style={styles.bookImage} source={{ uri: `https://www.libreriabonagura.it/wbresize.aspx?f=${book.isbn}.jpg&c=100&w=150` }} />
+                                            <View>
+                                                <Text>{book.titolo}</Text>
+                                                <Text>{book.autore}</Text>
+                                                <Text>Nuovo: {book.prezzo.toFixed(2)}€</Text>
+                                                <Text>Usato: {getPrezzoUsato(book.prezzo)}</Text>
+                                            </View>
+                                        </View>
+                                    ))}
+                                </ScrollView>
+                            }
+                        </>}
                     </CustomProgressStep>
                 </CustomProgressSteps>
             </View>
